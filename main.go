9475dfe555af88c09ab9d00dbf3985647296cc72
igorcoding <igorcoding@gmail.com>
2015-09-06 02:34:31 +0300

    xor recognition works

--- main.go
+++ main.go
@@ -7,63 +7,62 @@ import (
 
 func main() {
 
-	trainSet := []ffnet.TrainExample{
-		ffnet.NewTrainExample([]float64{
-			1, 0, 0,
-			0, 1, 0,
-			0, 0, 1}, []float64{1}),
-		ffnet.NewTrainExample([]float64{
-			0, 0, 1,
-			0, 1, 0,
-			1, 0, 0}, []float64{1}),
-
-		ffnet.NewTrainExample([]float64{
-			1, 1, 0,
-			0, 1, 0,
-			0, 0, 1}, []float64{0}),
-		ffnet.NewTrainExample([]float64{
-			0, 0, 1,
-			0, 1, 0,
-			0, 0, 1}, []float64{0}),
-		ffnet.NewTrainExample([]float64{
-			0, 1, 0,
-			0, 1, 0,
-			0, 1, 0}, []float64{0}),
-		ffnet.NewTrainExample([]float64{
-			1, 1, 1,
-			1, 1, 1,
-			1, 1, 1}, []float64{0}),
-		ffnet.NewTrainExample([]float64{
-			1, 1, 0,
-			0, 1, 0,
-			0, 1, 1}, []float64{0}),
-		ffnet.NewTrainExample([]float64{
-			1, 0, 1,
-			0, 1, 0,
-			1, 0, 1}, []float64{0}),
-	}
-
 //	trainSet := []ffnet.TrainExample{
-//		ffnet.TrainExample{Input:[]float64{0, 0}, Output:[]float64{0}},
-//		ffnet.TrainExample{Input:[]float64{0, 1}, Output:[]float64{1}},
-//		ffnet.TrainExample{Input:[]float64{1, 0}, Output:[]float64{1}},
-//		ffnet.TrainExample{Input:[]float64{1, 1}, Output:[]float64{0}},
+//		ffnet.NewTrainExample([]float64{
+//			1, 0, 0,
+//			0, 1, 0,
+//			0, 0, 1}, []float64{1}),
+//		ffnet.NewTrainExample([]float64{
+//			0, 0, 1,
+//			0, 1, 0,
+//			1, 0, 0}, []float64{1}),
+//
+//		ffnet.NewTrainExample([]float64{
+//			1, 1, 0,
+//			0, 1, 0,
+//			0, 0, 1}, []float64{0}),
+//		ffnet.NewTrainExample([]float64{
+//			0, 0, 1,
+//			0, 1, 0,
+//			0, 0, 1}, []float64{0}),
+//		ffnet.NewTrainExample([]float64{
+//			0, 1, 0,
+//			0, 1, 0,
+//			0, 1, 0}, []float64{0}),
+//		ffnet.NewTrainExample([]float64{
+//			1, 1, 1,
+//			1, 1, 1,
+//			1, 1, 1}, []float64{0}),
+//		ffnet.NewTrainExample([]float64{
+//			1, 1, 0,
+//			0, 1, 0,
+//			0, 1, 1}, []float64{0}),
+//		ffnet.NewTrainExample([]float64{
+//			1, 0, 1,
+//			0, 1, 0,
+//			1, 0, 1}, []float64{0}),
 //	}
 
+	trainSet := []ffnet.TrainExample{
+		ffnet.TrainExample{Input:[]float64{0, 0}, Output:[]float64{0}},
+		ffnet.TrainExample{Input:[]float64{0, 1}, Output:[]float64{1}},
+		ffnet.TrainExample{Input:[]float64{1, 0}, Output:[]float64{1}},
+		ffnet.TrainExample{Input:[]float64{1, 1}, Output:[]float64{0}},
+	}
 
 
-	conf := &ffnet.FFNetConf{Layers: []int32{9,2,1},
-	                         Alpha: 0.2,
+
+	conf := &ffnet.FFNetConf{Layers: []int32{2,3,1},
+	                         Alpha: 0.8,
 							 Eta: 0.1,
 							 Regularization: 0.8,
-//							 Bias: true,
+							 Bias: false,
 							 Iterations: 10000}
 	net := ffnet.BuildFFNet(conf)
 	net.Train(trainSet)
 
 	for t := range(trainSet) {
 		fmt.Println(trainSet[t].Input, "=>", net.Predict(trainSet[t].Input))
-		fmt.Println("-------")
 	}
 
 	fmt.Println(net)
